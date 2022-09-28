c06e76395754c46234f93fca4214aa41bf98dc68
happi89 <farhaanp99@gmail.com>
2022-09-28 08:01:14 -0400

    added definitions

---
+++ src/components/Block.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+
+export interface Definition {
+	partOfSpeech: string;
+	definitions: { definition: string }[];
+	synonyms: string[];
+}
+
+export interface Word {
+	word: string;
+	phonetic: string;
+	meanings: Definition[];
+}
+
+const Block = ({ word }: { word: Word }) => {
+	console.log(word);
+	return (
+		<>
+			{' '}
+			{word?.meanings?.map((m: Definition, i: number) => {
+				return (
+					<div key={i} className='mt-8'>
+						<p className='font-bold text-2xl'>{m.partOfSpeech}</p>
+						<ul>
+							{m.definitions?.map(
+								(
+									d: {
+										definition: string;
+									},
+									i: number
+								) => (
+									<li key={i} className='mt-2'>
+										{i} - {d.definition}
+									</li>
+								)
+							)}
+						</ul>
+						{m?.synonyms.length > 0 ? (
+							<>
+								<p className='font-bold text-xl mt-4'>synonyms</p>
+								<ul>
+									{m.synonyms?.map((s: string, i: number) => (
+										<Link href={`/${s}`} key={i}>
+											<a className='mr-2 hover:link cursor-pointer'>{s}</a>
+										</Link>
+									))}
+								</ul>
+								<div className='divider'></div>
+							</>
+						) : (
+							''
+						)}
+					</div>
+				);
+			})}
+		</>
+	);
+};
+
+export default Block;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,7 @@ import WordForm from './WordForm';
 export function Navbar({}) {
 	return (
 		<main className='container mx-auto flex flex-col items-center p-4'>
-			<h1 className='text-5xl leading-normal font-extrabold text-gray-700'>
-				Dictionary
-			</h1>
+			<h1 className='text-5xl leading-normal font-extrabold'>Dictionary</h1>
 			<WordForm />
 		</main>
 	);
